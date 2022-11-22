ff4354c06f33f0bee533c329505e82a95a67d1d4
Hindax <haar162@more2learn.de>
2022-11-22 12:54:14 +0100

    Class vs Objekt

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,18 +1,36 @@
 package com.cc.java;
 
 public class App {
-    
-    public static void main(String[] args) {
-        new Cat();
 
+    public static void main(String[] args) {
+
+        /**Typkonvertierung  */
+        App.output(String.valueOf(2)); // int --> String
+        App.output(Integer.toString(2)); // int--> String
+        App.output("Das ist ein Ziffer: " + 2); // int --> String
+
+
+        Cat cat = new Cat();
+        output("Blick von aussen " + cat);
+
+        cat.tellYourAddress();
+
+        output("-----------------");
+
+        Cat cat1 = new Cat();
+        output("Blink von aussen " + cat1);
+        
+        cat1.tellYourAddress();
+
+        App.tellYourAddress();
 
     }
 
-    public static void output( String outputStr) {
+    public static void output(String outputStr) {
         System.out.println(outputStr);
     }
 
-
-
+    public static void tellYourAddress() {
+        System.out.println("Hallo!");
+    }
 }
-
