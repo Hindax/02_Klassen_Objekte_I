94f4c4bc23387f1ad0f583468cac693f3dcf3f94
Hindax <haar162@more2learn.de>
2022-11-23 10:10:34 +0100

    set attributes

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,7 +1,24 @@
 package com.cc.java;
 
 public class Cat {
+
+   public String name = "nobody";
+   public String furColor;
+   public int age;
     
+   public String tellYourName() {
+      return this.name;
+   }
+
+   public String tellYourFurColor() {
+      return furColor;
+
+   }
+   public int tellYourAge(){
+      return this.age;
+   }
+
+
 public void tellYourAddress(){
    // Aufruf der Methode output() in der KLASSE App
    // this ist die Referenz des Objektes auf sich selbst
