6b857374646c92972c71bd5ef9627171f2c830c1
Hindax <haar162@more2learn.de>
2022-11-23 12:21:57 +0100

    private

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,15 +2,10 @@ package com.cc.java;
 
 public class Cat {
 
-   public String name = "nobody";
-   public String furColor;
-   public int age;
+   private String name = "nobody";
+   private String furColor;
+   private int age;
     
-      // public Cat(String name, String furColor, int age) {
-      //   this.name = name;
-      //   this.furColor = furColor;
-      //   this.age = age;
-      // }
 
       public Cat(String name, String furColor, int age) {
          this.name = name;
@@ -18,29 +13,30 @@ public class Cat {
          this.age = age;
       }
 
-   public String tellYourName() {
-      return this.name;
-   }
+      public String getName() {
+         return name;
+      }
 
-   public String tellYourFurColor() {
-      return furColor;
+      public String getFurColor() {
+         return furColor;
+      }
 
-   }
-   public int tellYourAge(){
-      return this.age;
-   }
+      public void setAge(int age) {
+         this.age = age;
+      }
 
+      public int getAge() {
+         return age;
+      }
 
+      public void setName(String name) {
+         this.name = name;
+      }
 
+      public void setFurColor(String furColor) {
+         this.furColor = furColor;
+      }
 
-public void tellYourAddress(){
-   // Aufruf der Methode output() in der KLASSE App
-   // this ist die Referenz des Objektes auf sich selbst
-    App.output("Blick von innen: " + this);
-}
-
-
-
-
+       
     
 }
