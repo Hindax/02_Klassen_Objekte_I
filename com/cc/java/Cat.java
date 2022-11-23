7b4976c87486fc255f3e8c4a5bd8059b5c9bbc2c
Hindax <haar162@more2learn.de>
2022-11-23 08:28:55 +0100

    Copied update

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,11 +1,15 @@
 package com.cc.java;
 
 public class Cat {
-
+    
 public void tellYourAddress(){
-   System.out.println("Blick von innen: " + this );
+   // Aufruf der Methode output() in der KLASSE App
+   // this ist die Referenz des Objektes auf sich selbst
+    App.output("Blick von innen: " + this);
 }
 
 
 
+
+    
 }
